ccaa864bf30972904b26258b4723d5490b21a395
satya1234msn <satyanivas2345@gmail.com>
2026-06-13 09:42:20 +0530

    clean up unused files

--- server/src/products/routes/creatorProduct.routes.js
+++ server/src/products/routes/creatorProduct.routes.js
@@ -2,13 +2,9 @@ const router = require("express").Router();
 const { authenticate } = require("../../auth/middleware/auth.middleware");
 
 // Individual Controller imports (Option B)
-const { createAndLinkProduct } = require("../controllers/createAndLinkProduct.controller");
 const { getMyProducts } = require("../controllers/getMyProducts.controller");
 const { getPublicProducts } = require("../controllers/getPublicProducts.controller");
 const { deleteProduct } = require("../controllers/deleteProduct.controller");
-
-// POST /api/creator-products/create-and-link (Private)
-router.post("/create-and-link", authenticate, createAndLinkProduct);
 
 // GET /api/creator-products/my (Private)
 router.get("/my", authenticate, getMyProducts);
